f7d92503b755400a575924127a272b6de9ff91ce
Etienne Chauchot <echauchot@apache.org>
2023-02-14 17:33:58 +0100

    simplify cassandra enumerator state serializer

--- flink-connector-cassandra/src/main/java/org/apache/flink/connector/cassandra/source/enumerator/CassandraEnumeratorStateSerializer.java
+++ flink-connector-cassandra/src/main/java/org/apache/flink/connector/cassandra/source/enumerator/CassandraEnumeratorStateSerializer.java
@@ -21,10 +21,12 @@ package org.apache.flink.connector.cassandra.source.enumerator;
 import org.apache.flink.connector.cassandra.source.split.CassandraSplit;
 import org.apache.flink.connector.cassandra.source.split.CassandraSplitSerializer;
 import org.apache.flink.core.io.SimpleVersionedSerializer;
-import org.apache.flink.core.memory.DataInputDeserializer;
-import org.apache.flink.core.memory.DataOutputSerializer;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
@@ -34,8 +36,6 @@ public class CassandraEnumeratorStateSerializer
 
     public static final CassandraEnumeratorStateSerializer INSTANCE =
             new CassandraEnumeratorStateSerializer();
-    private static final ThreadLocal<DataOutputSerializer> SERIALIZER_CACHE =
-            ThreadLocal.withInitial(() -> new DataOutputSerializer(64));
     public static final int CURRENT_VERSION = 0;
 
     @Override
@@ -45,37 +45,44 @@ public class CassandraEnumeratorStateSerializer
 
     @Override
     public byte[] serialize(CassandraEnumeratorState cassandraEnumeratorState) throws IOException {
-        final DataOutputSerializer out = SERIALIZER_CACHE.get();
-        final Queue<CassandraSplit> unassignedSplits =
-                cassandraEnumeratorState.getUnassignedSplits();
-        out.writeInt(unassignedSplits.size());
-        for (CassandraSplit cassandraSplit : unassignedSplits) {
-            final byte[] serializedSplit =
-                    CassandraSplitSerializer.INSTANCE.serialize(cassandraSplit);
-            out.writeInt(serializedSplit.length);
-            out.write(serializedSplit);
+        try (final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                final ObjectOutputStream objectOutputStream =
+                        new ObjectOutputStream(byteArrayOutputStream)) {
+            final Queue<CassandraSplit> unassignedSplits =
+                    cassandraEnumeratorState.getUnassignedSplits();
+            objectOutputStream.writeInt(unassignedSplits.size());
+            for (CassandraSplit cassandraSplit : unassignedSplits) {
+                final byte[] serializedSplit =
+                        CassandraSplitSerializer.INSTANCE.serialize(cassandraSplit);
+                objectOutputStream.writeInt(serializedSplit.length);
+                objectOutputStream.write(serializedSplit);
+            }
+            objectOutputStream.flush();
+            return byteArrayOutputStream.toByteArray();
         }
-        final byte[] result = out.getCopyOfBuffer();
-        out.clear();
-        return result;
     }
 
     @Override
     public CassandraEnumeratorState deserialize(int version, byte[] serialized) throws IOException {
-        final DataInputDeserializer in = new DataInputDeserializer(serialized);
-        Queue<CassandraSplit> unassignedSplits = new ArrayDeque<>();
-        final int unassignedSplitsSize = in.readInt();
-        for (int i = 0; i < unassignedSplitsSize; i++) {
-            final int splitSize = in.readInt();
-            final byte[] splitBytes = new byte[splitSize];
-            if (in.read(splitBytes) == -1) {
-                throw new IOException("EOF received while deserializing CassandraEnumeratorState");
+        try (final ByteArrayInputStream byteArrayInputStream =
+                        new ByteArrayInputStream(serialized);
+                final ObjectInputStream objectInputStream =
+                        new ObjectInputStream(byteArrayInputStream)) {
+            Queue<CassandraSplit> unassignedSplits = new ArrayDeque<>();
+            final int unassignedSplitsSize = objectInputStream.readInt();
+            for (int i = 0; i < unassignedSplitsSize; i++) {
+                final int splitSize = objectInputStream.readInt();
+                final byte[] splitBytes = new byte[splitSize];
+                if (objectInputStream.read(splitBytes) == -1) {
+                    throw new IOException(
+                            "EOF received while deserializing CassandraEnumeratorState");
+                }
+                final CassandraSplit split =
+                        CassandraSplitSerializer.INSTANCE.deserialize(
+                                CassandraSplitSerializer.CURRENT_VERSION, splitBytes);
+                unassignedSplits.add(split);
             }
-            final CassandraSplit split =
-                    CassandraSplitSerializer.INSTANCE.deserialize(
-                            CassandraSplitSerializer.CURRENT_VERSION, splitBytes);
-            unassignedSplits.add(split);
+            return new CassandraEnumeratorState(unassignedSplits);
         }
-        return new CassandraEnumeratorState(unassignedSplits);
     }
 }
